ee5d362ce7ad838cc892dc9abc61b50342a824d3
chamcbs <chamudithacbs2@gmail.com>
2023-01-31 23:04:13 +0530

    Initiation

---
+++ app/src/main/java/lk/ac/kln/MainActivity.kt
@@ -0,0 +1,11 @@
+package lk.ac.kln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
