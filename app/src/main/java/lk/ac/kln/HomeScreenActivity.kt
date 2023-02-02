7b4f6b32ac2e782cfc84b8cbf650ed9e0cfff37c
chamcbs <chamudithacbs2@gmail.com>
2023-02-02 23:59:46 +0530

    add home_screen layout

---
+++ app/src/main/java/lk/ac/kln/HomeScreenActivity.kt
@@ -0,0 +1,11 @@
+package lk.ac.kln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
