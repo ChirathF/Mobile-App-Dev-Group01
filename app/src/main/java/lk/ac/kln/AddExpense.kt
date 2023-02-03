0665ccc50a798352b702aa756c77b6ffce161c4e
BaskaranAathithyan <itsthiyan11@gmail.com>
2023-02-03 11:46:53 +0530

    Add income, expense layout file

---
+++ app/src/main/java/lk/ac/kln/AddExpense.kt
@@ -0,0 +1,11 @@
+package lk.ac.kln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddExpense : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_expense)
+    }
+}
