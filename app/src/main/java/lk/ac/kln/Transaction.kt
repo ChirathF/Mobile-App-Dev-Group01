d425d70af3abfceaf4d6a9bdd04ccfbee0f0b52c
gayasha754 <wanigase-im18089@stu.kln.ac.lk>
2023-02-07 22:50:36 +0530

    Add Transaction Dao

--- app/src/main/java/lk/ac/kln/Transaction.kt
+++ app/src/main/java/lk/ac/kln/Transaction.kt
@@ -3,8 +3,8 @@ package lk.ac.kln
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "incomes")
-data class Income(
+@Entity(tableName = "transactions")
+data class Transaction(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val Label:String,
     val Amount: Double,
