2ffacdcdbd5a2ad40f27f490153ce7a0770e233d
gayasha754 <wanigase-im18089@stu.kln.ac.lk>
2023-02-07 10:18:26 +0530

    Expense Dao

--- app/src/main/java/lk/ac/kln/ExpenseDao.kt
+++ app/src/main/java/lk/ac/kln/ExpenseDao.kt
@@ -4,5 +4,16 @@ import androidx.room.*
 
 @Dao
 interface ExpenseDao {
+    @Query("SELECT * from expenses")
+    fun getAll(): List<Expense>
+
+    @Insert
+    fun insertAll(vararg expense: Expense)
+
+    @Delete
+    fun delete(vararg expense: Expense)
+
+    @Update
+    fun update(vararg expense: Expense)
 
 }
